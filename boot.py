26b741887d8958a0cfc38298cd7c1d04efc94f46
Artem Pulkin <gpulkin@gmail.com>
2021-05-23 00:56:47 +0200

    boot.py: cleanup

--- boot.py
+++ boot.py
@@ -1,6 +1,6 @@
 from machine import Pin, I2C
-i2c = I2C(scl=Pin(22), sda=Pin(21))
-i2c.writeto_mem(0x35, 0x33, b"\xC0")
+I2C(scl=Pin(22), sda=Pin(21)).writeto_mem(0x35, 0x33, b"\xC0")
+del Pin, I2C
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
